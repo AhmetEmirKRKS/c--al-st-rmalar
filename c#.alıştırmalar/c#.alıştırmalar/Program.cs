82df421c67fb12705559b27c73a85eb90ed9df0b
AhmetEmirKRKS <ahmetemirkrks001@gmail.com>
2023-09-12 16:40:19 +0300

    c# alistirmalar
    c# başlangıç konsol örnekleri

---
+++ c#.alıştırmalar/c#.alıştırmalar/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_.alıştırmalar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+          {
+            string isim,soyisim,sehir;
+            int yas;
+            Console.WriteLine("Merhaba");
+            Console.WriteLine("isminiz:");
+            isim= Console.ReadLine();
+            Console.WriteLine("Soyisminiz:");
+            soyisim= Console.ReadLine();
+            Console.WriteLine("Yasadiginiz sehir");
+            sehir= Console.ReadLine();
+            Console.WriteLine("Yasiniz kac");
+            yas=int.Parse(Console.ReadLine());
+            Console.WriteLine(isim);
+            Console.WriteLine(soyisim);
+            Console.WriteLine(sehir);
+            Console.WriteLine(yas);
+            Console.ReadLine();
+            Console.Clear();    
+
+        }
+    }
+}
